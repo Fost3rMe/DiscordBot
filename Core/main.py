f48020474808f1d9bee2373d2be22e894ac9fe2d
Fost3rMe <fosterme@yandex.ru>
2021-12-09 17:50:35 +1000

    Add 2 functions

--- Core/main.py
+++ Core/main.py
@@ -2,5 +2,18 @@ import discord
 from discord.ext import commands
 from config import settings
 
-@bot.event
+bot = commands.Bot(command_prefix=settings['prefix'])
 
+
+@bot.command()
+async def hello(ctx):
+    author = ctx.message.author
+    await ctx.send('{}, hi'.format(author.mention))
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says hello when someone joined the chat"""
+    await ctx.send('{} вкатился в чат'.format(member))
+
+bot.run(settings['token'])
